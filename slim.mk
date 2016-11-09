69f6b9826a326c4d0d5267cc0dcd63e2698578b1
Paul Keith <javelinanddart@gmail.com>
2016-11-09 14:36:05 -0600

    CM -> Slim
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- slim.mk
+++ slim.mk
@@ -8,14 +8,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/nextbit/ether/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_ether
+PRODUCT_NAME := slim_ether
 BOARD_VENDOR := nextbit
 TARGET_VENDOR := nextbit
 PRODUCT_DEVICE := ether
