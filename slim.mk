0e32c4cbd602c4f31604fe3e08d265b8c2fe7bde
Daniel Koman <dankoman30@gmail.com>
2017-01-05 01:35:00 +0100

    swap dimensions
    Change-Id: I28fee970d4147aa3ad355f2adb98fe49198f15a3

--- slim.mk
+++ slim.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
 
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
